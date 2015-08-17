e46bfdb12094486aefecb3b9644da7fddfdeb6fe
Renato Botelho <renato@netgate.com>
2015-08-17 13:52:52 -0300

    Importing pfSense patch altq_max_queues.diff

--- sys/contrib/altq/altq/altq_hfsc.h
+++ sys/contrib/altq/altq/altq_hfsc.h
@@ -49,7 +49,7 @@ struct service_curve {
 
 /* special class handles */
 #define	HFSC_NULLCLASS_HANDLE	0
-#define	HFSC_MAX_CLASSES	64
+#define	HFSC_MAX_CLASSES	2048
 
 /* hfsc class flags */
 #define	HFCF_RED		0x0001	/* use RED */
