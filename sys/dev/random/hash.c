9c0a3ee89ffa68fc04d6f19e203bfa7f31ee2937
Luiz Otavio O Souza <luiz@netgate.com>
2015-09-15 13:48:54 -0500

    MFC r274340:
    Constify the AES code and propagate to consumers.  This allows us to
    update the Fortuna code to use SHAd-256 as defined in FS&K.
    
    Approved by:    so (self)
    
    MFC r274341:
    
    I just realized that the previous commit message makes no sense: the
    first sentence should have read "Constify the AES and SHA-256 code and
    wrappers".  This allows us to feed zero_region (which is const) to the
    hash function during reseeding and thereby implement the FS&K version of
    SHAd-256 instead of the older F&S version.
    
    Approved by:    so (self)
    
    TAG:	IPSEC-HEAD
    Issue:	#4841

--- sys/dev/random/hash.c
+++ sys/dev/random/hash.c
@@ -45,7 +45,7 @@ randomdev_hash_init(struct randomdev_hash *context)
 
 /* Iterate the hash */
 void
-randomdev_hash_iterate(struct randomdev_hash *context, void *data, size_t size)
+randomdev_hash_iterate(struct randomdev_hash *context, const void *data, size_t size)
 {
 	SHA256_Update(&context->sha, data, size);
 }
@@ -64,7 +64,7 @@ randomdev_hash_finish(struct randomdev_hash *context, void *buf)
  * data. Use CBC mode for better avalanche.
  */
 void
-randomdev_encrypt_init(struct randomdev_key *context, void *data)
+randomdev_encrypt_init(struct randomdev_key *context, const void *data)
 {
 	rijndael_cipherInit(&context->cipher, MODE_CBC, NULL);
 	rijndael_makeKey(&context->key, DIR_ENCRYPT, KEYSIZE*8, data);
@@ -75,7 +75,7 @@ randomdev_encrypt_init(struct randomdev_key *context, void *data)
  * a multiple of BLOCKSIZE.
  */
 void
-randomdev_encrypt(struct randomdev_key *context, void *d_in, void *d_out, unsigned length)
+randomdev_encrypt(struct randomdev_key *context, const void *d_in, void *d_out, unsigned length)
 {
 	rijndael_blockEncrypt(&context->cipher, &context->key, d_in, length*8, d_out);
 }
