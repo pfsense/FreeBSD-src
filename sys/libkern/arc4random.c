4878ae98004f6dbf5a707dc5fe5f0f06d5c641e5
jmutkawoa <jmutkawoa@hackers.mu>
2017-03-19 10:28:53 -0400

    MFC: r315225 Operation C.R - hackers.mu As per Cryptographic Requirements published on wikileaks on March 2017, we discard more bytes (3072 byte)of the first keystream to reduce the possibility of non-random bytes.

--- sys/libkern/arc4random.c
+++ sys/libkern/arc4random.c
@@ -82,7 +82,7 @@ arc4_randomstir (void)
 	 * paper "Weaknesses in the Key Scheduling Algorithm of RC4"
 	 * by Fluher, Mantin, and Shamir.  (N = 256 in our case.)
 	 */
-	for (n = 0; n < 256*4; n++)
+	for (n = 0; n < 768*4; n++)
 		arc4_randbyte();
 	mtx_unlock(&arc4_mtx);
 }
