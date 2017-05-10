99cf3bb49d44b0177cdd62b2c31fc263876af482
Luiz Souza <luiz@netgate.com>
2017-05-10 16:00:24 -0500

    Fix an obvious bug.
    Pointy-hat to me.
    
    (cherry picked from commit bbad63dee2950429a7ae71d65fddf380d7c1204d)

--- sys/dev/neta/if_mvneta.c
+++ sys/dev/neta/if_mvneta.c
@@ -627,7 +627,7 @@ mvneta_attach(device_t self)
 #else /* !MVNETA_MULTIQUEUE */
 	ifp->if_start = mvneta_start;
 	ifp->if_snd.ifq_drv_maxlen = 0;
-	IFQ_SET_MAXLEN(&ifp->if_snd, ifp->if_snd.ifq_drv_maxlen);
+	IFQ_SET_MAXLEN(&ifp->if_snd, MVNETA_TX_RING_CNT - 1);
 	IFQ_SET_READY(&ifp->if_snd);
 #endif
 	ifp->if_init = mvneta_init;
