f0cf55d704ac9613df0c33017557d6f57dd1ac34
Luiz Souza <luiz@netgate.com>
2017-06-20 19:03:27 -0500

    Default to IFM_1000_T.
    (cherry picked from commit 74ccb14cdd534dffa4334b1111aa3d64d0c89419)

--- sys/dev/neta/if_mvneta.c
+++ sys/dev/neta/if_mvneta.c
@@ -2118,12 +2118,12 @@ mvneta_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 		    IFM_SUBTYPE(ifr->ifr_media) == IFM_2500_T ||
 		    IFM_SUBTYPE(ifr->ifr_media) == IFM_2500_KX) &&
 		    (ifr->ifr_media & IFM_FDX) == 0) {
-			if (IFM_SUBTYPE(ifr->ifr_media) == IFM_1000_T)
-				mediatype = "1000Base-T";
-			else if (IFM_SUBTYPE(ifr->ifr_media) == IFM_2500_T)
+			if (IFM_SUBTYPE(ifr->ifr_media) == IFM_2500_T)
 				mediatype = "2500Base-T";
 			else if (IFM_SUBTYPE(ifr->ifr_media) == IFM_2500_KX)
 				mediatype = "2500Base-KX";
+			else
+				mediatype = "1000Base-T";
 			device_printf(sc->dev,
 			    "%s half-duplex unsupported\n", mediatype);
 			error = EINVAL;
@@ -2460,12 +2460,12 @@ mvneta_update_media(struct mvneta_softc *sc, int media)
 		    IFM_SUBTYPE(media) == IFM_2500_T ||
 		    IFM_SUBTYPE(media) == IFM_2500_KX) {
 			if ((media & IFM_FDX) == 0) {
-				if (IFM_SUBTYPE(media) == IFM_1000_T)
-					type = "1000Base-T";
-				else if (IFM_SUBTYPE(media) == IFM_2500_T)
+				if (IFM_SUBTYPE(media) == IFM_2500_T)
 					type = "2500Base-T";
 				else if (IFM_SUBTYPE(media) == IFM_2500_KX)
 					type = "2500Base-KX";
+				else
+					type = "1000Base-T";
 				device_printf(sc->dev,
 				    "%s half-duplex unsupported\n", type);
 				err = EINVAL;
