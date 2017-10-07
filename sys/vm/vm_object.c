77c23f6c7df09a861b606acaa485ba1ab8883533
alc <alc@FreeBSD.org>
2017-10-07 17:20:31 +0000

    MFC r323973,324087   Optimize vm_page_try_to_free().  Specifically, the call to pmap_remove_all()   can be avoided when the page's containing object has a reference count of   zero.  (If the object has a reference count of zero, then none of its pages   can possibly be mapped.)
      Address nearby style issues in vm_page_try_to_free(), and change its
      return type to "bool".
    
      Optimize vm_object_page_remove() by eliminating pointless calls to
      pmap_remove_all().  If the object to which a page belongs has no
      references, then that page cannot possibly be mapped.
    
    (cherry picked from commit 2d2427db5b735ecdb6fe8ad9251f524b4260bb6a)

--- sys/vm/vm_object.c
+++ sys/vm/vm_object.c
@@ -1934,7 +1934,8 @@ again:
 			goto again;
 		}
 		if (p->wire_count != 0) {
-			if ((options & OBJPR_NOTMAPPED) == 0)
+			if ((options & OBJPR_NOTMAPPED) == 0 &&
+			    object->ref_count != 0)
 				pmap_remove_all(p);
 			if ((options & OBJPR_CLEANONLY) == 0) {
 				p->valid = 0;
@@ -1951,12 +1952,13 @@ again:
 		KASSERT((p->flags & PG_FICTITIOUS) == 0,
 		    ("vm_object_page_remove: page %p is fictitious", p));
 		if ((options & OBJPR_CLEANONLY) != 0 && p->valid != 0) {
-			if ((options & OBJPR_NOTMAPPED) == 0)
+			if ((options & OBJPR_NOTMAPPED) == 0 &&
+			    object->ref_count != 0)
 				pmap_remove_write(p);
-			if (p->dirty)
+			if (p->dirty != 0)
 				continue;
 		}
-		if ((options & OBJPR_NOTMAPPED) == 0)
+		if ((options & OBJPR_NOTMAPPED) == 0 && object->ref_count != 0)
 			pmap_remove_all(p);
 		vm_page_free(p);
 	}
