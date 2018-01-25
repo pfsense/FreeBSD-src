16a2520e85dd8e13b1fc8e46f32fd2052c8a234f
Luiz Souza <luiz@netgate.com>
2018-01-25 13:49:16 -0200

    Make rtsold(8) work on VLANs.
    Ticket #4909.
    
    (cherry picked from commit 070fe5761940aede4ba68e28afd494effb2ec38e)

--- usr.sbin/rtsold/if.c
+++ usr.sbin/rtsold/if.c
@@ -248,6 +248,7 @@ lladdropt_length(struct sockaddr_dl *sdl)
 	switch (sdl->sdl_type) {
 	case IFT_ETHER:
 	case IFT_IEEE80211:
+	case IFT_L2VLAN:
 		return (ROUNDUP8(ETHER_ADDR_LEN + 2));
 	default:
 		return (0);
@@ -264,6 +265,7 @@ lladdropt_fill(struct sockaddr_dl *sdl, struct nd_opt_hdr *ndopt)
 	switch (sdl->sdl_type) {
 	case IFT_ETHER:
 	case IFT_IEEE80211:
+	case IFT_L2VLAN:
 		ndopt->nd_opt_len = (ROUNDUP8(ETHER_ADDR_LEN + 2)) >> 3;
 		addr = (char *)(ndopt + 1);
 		memcpy(addr, LLADDR(sdl), ETHER_ADDR_LEN);
