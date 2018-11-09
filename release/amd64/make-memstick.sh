4f85556a02f3f40967f5e9578457792c478f1889
Renato Botelho <renato@netgate.com>
2018-11-09 15:44:11 -0200

    Add a 3rd optional parameter to make-memstick.sh to add a FAT32 partition to memstick image

--- release/amd64/make-memstick.sh
+++ release/amd64/make-memstick.sh
@@ -15,8 +15,8 @@ set -e
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 export PATH
 
-if [ $# -ne 2 ]; then
-	echo "make-memstick.sh /path/to/directory /path/to/image/file"
+if [ $# -ne 2 && $# -ne 3 ]; then
+	echo "make-memstick.sh /path/to/directory /path/to/image/file [FAT32 partition]"
 	exit 1
 fi
 
@@ -30,6 +30,11 @@ if [ -e ${2} ]; then
 	exit 1
 fi
 
+unset fat32_partition
+if [ -n "${3}" -a -f "${3}" ]; then
+	fat32_partition="-p fat32:=${3}"
+fi
+
 echo '/dev/ufs/FreeBSD_Install / ufs ro,noatime 1 1' > ${1}/etc/fstab
 echo 'root_rw_mount="NO"' > ${1}/etc/rc.conf.local
 makefs -B little -o label=FreeBSD_Install -o version=2 ${2}.part ${1}
@@ -40,6 +45,7 @@ mkimg -s mbr \
     -b ${1}/boot/mbr \
     -p efi:=${1}/boot/boot1.efifat \
     -p freebsd:-"mkimg -s bsd -b ${1}/boot/boot -p freebsd-ufs:=${2}.part" \
+    ${fat32_partition} \
     -a 2 \
     -o ${2}
 rm ${2}.part
