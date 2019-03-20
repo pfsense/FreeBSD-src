da872a8c2a5a27eb38564186d0312253d1892ddb
Luiz Souza <luiz@netgate.com>
2019-03-20 17:43:40 -0300

    Rename the functions to avoid a name clashing.
    Fixes the build of the pf bsnmp module.
    
    Ticket:	#9382

--- usr.sbin/bsnmpd/modules/snmp_pf/pf_snmp.c
+++ usr.sbin/bsnmpd/modules/snmp_pf/pf_snmp.c
@@ -907,7 +907,7 @@ pf_tbladdr(struct snmp_context __unused *ctx, struct snmp_value __unused *val,
 }
 
 int
-pf_altq(struct snmp_context __unused *ctx, struct snmp_value *val,
+pf_altq_snmp(struct snmp_context __unused *ctx, struct snmp_value *val,
 	u_int sub, u_int __unused vindex, enum snmp_op op)
 {
 	asn_subid_t	which = val->var.subs[sub - 1];
@@ -940,7 +940,7 @@ pf_altq(struct snmp_context __unused *ctx, struct snmp_value *val,
 }
 
 int
-pf_altqq(struct snmp_context __unused *ctx, struct snmp_value *val,
+pf_altqq_snmp(struct snmp_context __unused *ctx, struct snmp_value *val,
 	u_int sub, u_int __unused vindex, enum snmp_op op)
 {
 	asn_subid_t	which = val->var.subs[sub - 1];
