2c7ab6a3c3f1abf031bf78d5d621bf0756aded33
Luiz Souza <luiz@netgate.com>
2020-10-02 13:00:50 -0300

    Fixes a few more data races and adds a debug print.
    Submitted by:	markj@

--- sys/netgraph/ng_l2tp.c
+++ sys/netgraph/ng_l2tp.c
@@ -1457,6 +1457,7 @@ ng_l2tp_seq_rack_timeout(node_p node, hook_p hook, void *arg1, int arg2)
 	struct l2tp_seq *const seq = &priv->seq;
 	struct mbuf *m;
 	u_int delay;
+	uint16_t ns;
 
 	/* Sanity check */
 	L2TP_SEQ_CHECK(seq);
@@ -1468,6 +1469,10 @@ ng_l2tp_seq_rack_timeout(node_p node, hook_p hook, void *arg1, int arg2)
 		mtx_unlock(&seq->mtx);
 		return;
 	}
+
+	if (seq->xwin[0] == NULL)
+		printf("empty transmit queue: ns %u rack %u active %d",
+		    seq->ns, seq->rack, callout_active(&seq->rack_timer));
 
 	priv->stats.xmitRetransmits++;
 
@@ -1490,11 +1495,12 @@ ng_l2tp_seq_rack_timeout(node_p node, hook_p hook, void *arg1, int arg2)
 
 	/* Retransmit oldest unack'd packet */
 	m = L2TP_COPY_MBUF(seq->xwin[0], M_NOWAIT);
+	ns = seq->ns++;
 	mtx_unlock(&seq->mtx);
 	if (m == NULL)
 		priv->stats.memoryFailures++;
 	else
-		ng_l2tp_xmit_ctrl(priv, m, seq->ns++);
+		ng_l2tp_xmit_ctrl(priv, m, ns);
 
 	/* callout_deactivate() is not needed here 
 	    as ng_callout() is getting called each time */
@@ -1512,8 +1518,8 @@ ng_l2tp_xmit_ctrl(priv_p priv, struct mbuf *m, u_int16_t ns)
 {
 	struct l2tp_seq *const seq = &priv->seq;
 	uint8_t *p;
-	u_int16_t session_id = 0;
 	int error;
+	uint16_t session_id = 0, nr;
 
 	mtx_lock(&seq->mtx);
 
@@ -1522,7 +1528,8 @@ ng_l2tp_xmit_ctrl(priv_p priv, struct mbuf *m, u_int16_t ns)
 	if (callout_active(&seq->xack_timer))
 		ng_uncallout(&seq->xack_timer, priv->node);
 
-	seq->xack = seq->nr;
+	nr = seq->nr;
+	seq->xack = nr;
 
 	mtx_unlock(&seq->mtx);
 
@@ -1577,8 +1584,8 @@ ng_l2tp_xmit_ctrl(priv_p priv, struct mbuf *m, u_int16_t ns)
 	p[7] = session_id & 0xff;
 	p[8] = ns >> 8;
 	p[9] = ns & 0xff;
-	p[10] = seq->nr >> 8;
-	p[11] = seq->nr & 0xff;
+	p[10] = nr >> 8;
+	p[11] = nr & 0xff;
 
 	/* Update sequence number info and stats */
 	priv->stats.xmitPackets++;
