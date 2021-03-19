404dcc194180b56f26448c24e7d7dcfad5fc7a13
Renato Botelho do Couto <renato@netgate.com>
2021-03-19 11:35:57 -0300

    Revert "if_wg: return to m_defrag() of incoming mbuf, sans leak"
    This reverts commit 626ab285e8008c82aaff988d20f37bff6eb9fa61.

--- sys/dev/if_wg/module/if_wg_session.c
+++ sys/dev/if_wg/module/if_wg_session.c
@@ -1911,12 +1911,13 @@ wg_input(struct mbuf *m0, int offset, struct inpcb *inpcb,
 	m_adj(m0, hlen);
 
 	/*
-	 * Ensure mbuf has at least enough contiguous data to peel off our
-	 * headers at the beginning.
+	 * Ensure mbuf is contiguous over full length of the packet.  This is
+	 * done so that we can directly read the handshake values in
+	 * wg_handshake, and so we can decrypt a transport packet by passing a
+	 * a single buffer to noise_remote_decrypt() in wg_decap.
 	 */
-	if ((m = m_defrag(m0, M_NOWAIT)) == NULL) {
+	if ((m = m_pullup(m0, m0->m_pkthdr.len)) == NULL) {
 		DPRINTF(sc, "DEFRAG fail\n");
-		m_freem(m0);
 		return;
 	}
 	data = mtod(m, void *);
