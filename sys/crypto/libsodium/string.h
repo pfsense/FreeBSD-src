e29a469e1b64673ba2ce92f58f4175fe07a82888
Renato Botelho do Couto <renato@netgate.com>
2021-03-19 11:42:02 -0300

    Revert "Import the Wireguard support for ifconfig(8)."
    This reverts commit 74cfbc94969c24cea6e4ab09a0ea5ae7f97c3d99.

--- sys/crypto/libsodium/string.h
+++ sys/crypto/libsodium/string.h
@@ -1,6 +1,2 @@
 /* This file is in the public domain */
 /* $FreeBSD$ */
-#include <sys/types.h>
-
-void    *memcpy(void * __restrict, const void * __restrict, size_t);
-void    *memset(void *, int, size_t);
