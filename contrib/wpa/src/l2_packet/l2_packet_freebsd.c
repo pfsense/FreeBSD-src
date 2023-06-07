2aa7566696a8a239523b8ae5aa2d5fdf228aa6b8
Christian McDonald <cmcdonald@netgate.com>
2023-06-07 17:28:06 +0000

    wpa_supplicant: support receiving VLAN 0 EAPOL frames. Implements #14457

--- contrib/wpa/src/l2_packet/l2_packet_freebsd.c
+++ contrib/wpa/src/l2_packet/l2_packet_freebsd.c
@@ -21,6 +21,7 @@
 #include <sys/sysctl.h>
 #endif /* __sun__ */
 
+#include <net/ethernet.h>
 #include <net/if.h>
 #include <net/if_dl.h>
 #include <net/route.h>
@@ -85,6 +86,7 @@ static void l2_packet_receive(int sock, void *eloop_ctx, void *sock_ctx)
 
 	if (pcap_next_ex(pcap, &hdr, &packet) == -1) {
 		wpa_printf(MSG_ERROR, "Error reading packet, has device disappeared?");
+		packet = NULL;
 		eloop_terminate();
 	}
 
@@ -98,6 +100,9 @@ static void l2_packet_receive(int sock, void *eloop_ctx, void *sock_ctx)
 	} else {
 		buf = (unsigned char *) (ethhdr + 1);
 		len = hdr->caplen - sizeof(*ethhdr);
+		/* handle 8021Q encapsulated frames */
+		if (ethhdr->h_proto == htons(ETH_P_8021Q))
+			buf += ETHER_VLAN_ENCAP_LEN; len -= ETHER_VLAN_ENCAP_LEN;
 	}
 	l2->rx_callback(l2->rx_callback_ctx, ethhdr->h_source, buf, len);
 }
@@ -126,10 +131,10 @@ static int l2_packet_init_libpcap(struct l2_packet_data *l2,
 	os_snprintf(pcap_filter, sizeof(pcap_filter),
 		    "not ether src " MACSTR " and "
 		    "( ether dst " MACSTR " or ether dst " MACSTR " ) and "
-		    "ether proto 0x%x",
+		    "( ether proto 0x%x or ( vlan 0 and ether proto 0x%x ) )",
 		    MAC2STR(l2->own_addr), /* do not receive own packets */
 		    MAC2STR(l2->own_addr), MAC2STR(pae_group_addr),
-		    protocol);
+		    protocol, protocol);
 	if (pcap_compile(l2->pcap, &pcap_fp, pcap_filter, 1, pcap_netp) < 0) {
 		fprintf(stderr, "pcap_compile: %s\n", pcap_geterr(l2->pcap));
 		return -1;
